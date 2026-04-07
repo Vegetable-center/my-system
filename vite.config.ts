4275ec82e83143e5135db3747db125259107ed30
Czn <2540719857@qq.com>
2026-04-07 17:34:23 +0800

    feat: get apiData

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,14 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
-      }
+      },
+      '/uploads': {
+        target: 'http://localhost:3000', // 后端 Nest 服务地址
+        changeOrigin: true, // 开启跨域
+        secure: false, // 开发环境关闭 HTTPS 验证
+        // 可选：重写路径（如果后端有前缀，比如 /api/uploads，就需要重写）
+        // rewrite: (path) => path.replace(/^\/uploads/, '/api/uploads'),
+      },
     }
   }
 })
